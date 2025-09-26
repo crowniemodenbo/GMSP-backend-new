f54095b0268b93ecfde12920bd8031d1b8314e84
crowniemodenbo <crownie.eme@modenbo.com>
2025-09-26 14:44:24 -0400

    secured app secrets

--- backend/settings.py
+++ backend/settings.py
@@ -21,10 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kgh$6-)=9=8_ckr*t)51q7035ok%b2l^z3j&!6%nhl%l^6i&f8'
+
+SECRET_KEY = config("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
