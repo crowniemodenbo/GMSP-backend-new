14929d10eefc01e0b5550f76e6e43bd312d023f1
crowniemodenbo <crownie.eme@modenbo.com>
2026-01-06 13:18:13 +0000

    Added cors allowed origins

--- backend/settings.py
+++ backend/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -57,7 +58,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 
 ]
 
@@ -172,6 +172,11 @@ REST_FRAMEWORK = {
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '196.13.252.30','gmspignite.aims.edu.gh']
 
 CORS_ALLOW_ALL_ORIGINS = True  # Allows all domains to access (useful for dev)
+
+CORS_ALLOWED_ORIGINS = [
+    "http://gmspignite.aims.edu.gh", 'http://127.0.0.1:8000', 'http://localhost:8000','http://196.13.252.30'
+]
+
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=30),  # or longer, as needed
